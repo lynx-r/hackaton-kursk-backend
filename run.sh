091e4e30dbf7419f5f74f0772dce6e38a7911e42
lynx-r <aleksey.p10n@gmail.com>
2019-08-05 12:04:05 +0300

    sec

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-PORT=8080 TOKEN_SECRET="S2{Ux(QSDpuF(qh_E{|T3W}w9{^rWd*tQf6()123" REALM_KEY=myrealm1234 REALM_NAME="Prod Realm via Digest Authentication" ORIGIN_URL="https://hackaton-kursk.shashki.online" java -jar ./build/libs/hackaton-kursk-backend-0.0.1-SNAPSHOT.jar
+source .env
+
+PORT=8080 TOKEN_SECRET=$TOKEN_SECRET REALM_KEY=$REALM_KEY REALM_NAME=$REALM_NAME ORIGIN_URL="https://hackaton-kursk.shashki.online" java -jar ./build/libs/hackaton-kursk-backend-0.0.1-SNAPSHOT.jar
 
